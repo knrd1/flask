09cb7fa977f7061a114cf41c19a070b8417edc5c
knrd1 <36317223+knrd1@users.noreply.github.com>
2018-02-10 20:26:20 +0000

    Update www.py

--- www.py
+++ www.py
@@ -1,10 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Wordl!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
