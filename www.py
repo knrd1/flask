0fce93437d88f10a3015229562aea81120886649
knrd1 <36317223+knrd1@users.noreply.github.com>
2018-02-13 00:27:34 +0000

    Update www.py

--- www.py
+++ www.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__hello_world__":
     app.run()
